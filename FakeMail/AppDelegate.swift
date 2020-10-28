014a07efd19ca0f8f14f6ce69b5b77e35f269c01
ploenne <ploenne@ploenne.net>
2020-10-28 20:22:13 +0100

    inital commit

---
+++ FakeMail/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FakeMail
+//
+//  Created by Thomas Ploentzke on 28.10.20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
